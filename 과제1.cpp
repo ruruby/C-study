9643b45b2557bf2c72d92179f4bcc4ae77282d86
ruruby <63354563+ruruby@users.noreply.github.com>
2020-04-14 08:28:23 +0900

    Add files via upload

---
+++ 과제1.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+	char name[100];
+	char birth[100];
+	string age;
+	string department;
+	string hobby;
+	cout << "이름을 입력하세요: ";
+	cin >> name;
+	cout << "생일을 입력하세요: "<<getchar();
+	cin.getline(birth, 100);
+	cout << "나이를 입력하세요: ";
+	getline(cin, age);
+	cout << "소속 학과를 입력하세요: ";
+	getline(cin, department);
+	cout << "취미를 입력하세요: ";
+	getline(cin, hobby);
+	cout << endl << endl;
+	cout << "====================================================";
+	cout << endl<<"<회원 정보>" << endl;
+	cout << "이름: " <<name<<endl;
+	cout << "생일: " << birth << endl;
+	cout << "나이: " << age<<endl;
+	cout << "소속 학과: " << department<<endl;
+	cout << "취미: " << hobby;
+}
